5ec12b5f61bb093c017532c915017692b413fca4
vishalchaurasiya9118 <vishal.chaurasiya@qable.io>
2024-05-24 16:59:05 +0530

    xyz

--- ui-tests/cypress/e2e/dragon/InvoiceQueue.cy.js
+++ ui-tests/cypress/e2e/dragon/InvoiceQueue.cy.js
@@ -206,7 +206,7 @@ describe("Invoice Queue", () => {
 
     // C76607 Marking an invoice as Paid using Manual Check (use Exsiting Docyt check)
 
-     it.only("C76607: Marking an invoice as Paid using Manual Check (use Exsiting Docyt check)",()=> {
+     it("C76607: Marking an invoice as Paid using Manual Check (use Exsiting Docyt check)",()=> {
         
         const invoice_number = "INV_" + time_helper.get_epoch_time();
         const current_date = time_helper.get_current_date_of_month();
@@ -238,7 +238,67 @@ describe("Invoice Queue", () => {
         invoice_queue_page.check_register_flow();
 
 
-        
      })
+
+     it.only("C18300: Cancel Docyt Check",()=>
+        {
+            {
+                const invoice_number = "INV_" + time_helper.get_epoch_time();
+                const current_date = time_helper.get_current_date_of_month();
+                const last_date_of_month = time_helper.get_last_date_of_current_month();
+                const amount = number_helper.get_random_number(100000);
+                const invoice = {
+                    "invoice_number": invoice_number,
+                    "payee": data['vendor2']['name'],
+                    "invoice_date": current_date,
+                    "due_date": last_date_of_month,
+                    "amount": amount,
+                    "address": data['vendor2']['city_pin'],
+                    "formatted_amount": number_helper.get_formatted_amount(amount)
+                }
+        
+                login_page.login_and_navigate_to_business_path(data['email'], data['password'], "ACCOUNTS_PAYABLE_INVOICE_QUEUE", data['test_business1_id'])
+                invoice_queue_page.verify_invoice_queue_page_displayed();
+        
+                
+                invoice_queue_page.click_add_invoice_button()
+                add_edit_invoice.verify_add_an_invoice_form_displayed()
+                add_edit_invoice.fill_add_invoice_form_and_submit(invoice)
+                invoice_queue_page.filter_by_invoice(invoice_number)
+                invoice_queue_page.verify_invoice_exists(invoice_number)
+        
+                
+                invoice_queue_page.verify_invoice_status(invoice_number, 'In Verification')
+                invoice_queue_page.click_first_row_amount_dropdown_option("Verify Data")
+                add_edit_invoice.verify_docyt_id_displayed();
+                add_edit_invoice.click_mark_as_verified_button();
+                cy.reload();
+                invoice_queue_page.filter_by_invoice(invoice_number)
+                invoice_queue_page.verify_invoice_status(invoice_number, 'Ready to Pay')
+                
+        +       invoice_queue_page.click_first_row_amount_dropdown_option("Pay by Docyt Check")
+                add_edit_invoice.click_continue_button_from_multiple_unpaid_invoices_detected_page()
+        +       add_edit_invoice.verify_docyt_id_displayed();
+                add_edit_invoice.verify_invoice_data_under_details_tab(invoice);
+        
+                // Pay by Docyt Check
+                add_edit_invoice.click_generate_check_button();
+                add_edit_invoice.verify_identity_and_generate_payment();
+                navigate_to_business_path("ACCOUNTS_PAYABLE_INVOICE_PAID", data['test_business1_id']);
+                invoice_queue_page.verify_invoice_queue_page_displayed();
+                invoice_queue_page.filter_by_invoice(invoice_number)
+                invoice_queue_page.verify_invoice_exists(invoice_number);
+        
+                invoice_queue_page.click_first_row_amount_dropdown_option("Cancel Payment");
+                add_edit_invoice.click_confirm_from_cancel_payment_modal();
+                navigate_to_business_path("ACCOUNTS_PAYABLE_INVOICE_QUEUE", data['test_business1_id']);
+                invoice_queue_page.verify_invoice_queue_page_displayed();
+                invoice_queue_page.filter_by_invoice(invoice_number)
+                invoice_queue_page.delete_invoice(invoice_number)
+        
+        }})
+
+        
+    
 
 })
