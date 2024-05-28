b1795122103f0175461381807137a6d4dd5956b5
vishalchaurasiya9118 <vishal.chaurasiya@qable.io>
2024-05-28 16:23:56 +0530

    C305218

---
+++ ui-tests/cypress/e2e/lion/banking_accounts/transactions.js
@@ -0,0 +1,3 @@
+const login_page  = require("../../lib/common/login/login");
+const { navigate_to_business_path } = require("../../lib/common/navigation/navigation");
+const data = Cypress.env('lion');
